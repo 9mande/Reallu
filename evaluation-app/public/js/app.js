2cae3b531b50819ea4e31c0a492856ab864e1df9
Minjae Gu <gudori888@naver.com>
2023-05-30 08:52:44 +0900

    gonna change to static website

--- evaluation-app/public/js/app.js
+++ evaluation-app/public/js/app.js
@@ -43,7 +43,7 @@ App = {
       App.web3Provider = new Web3.providers.HttpProvider(App.url);
     }
     web3 = new Web3(App.web3Provider);
-    ethereum.enable();
+    // ethereum.enable();
 
     return App.initContract();
   },
